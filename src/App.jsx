a36ebfc4630c4a0939b65e001bb7517c28d43740
Mohamed-Aljerjawi01 <mohamedahmedjerjawi@example.com>
2025-10-28 14:43:19 +0200

    About Component commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from'./components/navbar/Navbar.jsx'
 import Hero from'./components/hero/Hero.jsx'
 import Portfolio from'./components/portfolio/Portfolio.jsx'
+import About from'./components/about/About.jsx'
 
 function App(){
   return <>
     <Navbar/>
     <Hero/>
     <Portfolio/>
+    <About/>
   </>
 }
 
---
+++ src/components/about/About.jsx
@@ -0,0 +1,31 @@
+import styleAbout from './about.module.css'
+
+function About() {
+    // JS COde 
+
+    // HTML Code 
+    return <>
+        <div className={styleAbout.one} id='about'>
+            <h2 className={styleAbout.title}>About</h2>
+            <span className={styleAbout.span}>***</span>
+            <div className={styleAbout.flex}>
+                <div className={styleAbout.div1}>
+                    <p className={styleAbout.para}>
+                        Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.
+                    </p>
+                </div>
+                <div className={styleAbout.div2}>
+                    <p className={styleAbout.para}>
+                        You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!
+                    </p>
+                </div>
+            </div>
+            <button className={styleAbout.btn}>
+                <svg class={`svg-inline--fa fa-download me-2 ${styleAbout.svg}`} aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"></path></svg>
+                Free Download!
+            </button>
+        </div>
+    </>
+}
+
+export default About;
